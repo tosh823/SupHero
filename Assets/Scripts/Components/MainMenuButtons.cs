f0f84f808e2b99256cabe4680ed816509f0723c6
AaroM2 <n3ylaa00@students.oamk.fi>
2016-03-18 16:16:32 +0200

    Menu UI changes
    Menu UI now yellow/gold and start button changes scene to the level
    scene.

---
+++ Assets/Scripts/Components/MainMenuButtons.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public void ChangeScene (string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+		
+}

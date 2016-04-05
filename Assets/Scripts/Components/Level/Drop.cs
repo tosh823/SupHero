7444e3ac0318b4f77cfdbf0cd6e19546ef3950a5
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-04-05 13:45:32 +0300

    Drop class

---
+++ Assets/Scripts/Components/Level/Drop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SupHero.Components {
+    public class Drop : MonoBehaviour {
+
+        public GameObject dropItem;
+
+        void Start() {
+            dropItem = Instantiate(dropItem) as GameObject;
+            dropItem.transform.SetParent(transform);
+            dropItem.transform.position = transform.position;
+            dropItem.transform.Translate(new Vector3(0f, 0f, 0.5f));
+            dropItem.transform.eulerAngles = new Vector3(-90f, 0f, 0f);
+        }
+
+        void Update() {
+            transform.Rotate(new Vector3(0f, 20f, 0f) * Time.deltaTime);
+        }
+    }
+}

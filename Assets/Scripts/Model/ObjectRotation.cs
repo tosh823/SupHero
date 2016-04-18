b61b5a521d20dc6606f45880f8469ba210892186
AaroM2 <n3ylaa00@students.oamk.fi>
2016-04-18 15:07:58 +0300

    Pickup prefabs
    Added pickup models and created prefabs from those. Functionality not
    implemented.
    Also modified weapon textures to be more brighter on weapon models.

---
+++ Assets/Scripts/Model/ObjectRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour {
+
+	public float turnSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotate the object around its local Y axis at 1 degree per second
+		//transform.Rotate(Vector3.right * Time.deltaTime);
+
+		// ...also rotate around the World's Y axis
+		transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed, Space.World);
+
+	}
+}

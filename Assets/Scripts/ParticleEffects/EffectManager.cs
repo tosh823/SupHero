4b3ff6f6e1341a54aeaed24adc03c80574c3b4fd
AaroM2 <n3ylaa00@students.oamk.fi>
2016-04-20 15:03:29 +0300

    Healing effect
    Created a healing effect to test scene. Animation needs to be triggered
    when picking up a health pack.

---
+++ Assets/Scripts/ParticleEffects/EffectManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectManager : MonoBehaviour {
+
+	public GameObject[] effects;
+
+	public void PlayEffect(int currentEffect)
+	{
+		effects [currentEffect].GetComponent<Animator> ().SetTrigger ("activated");
+	}
+}

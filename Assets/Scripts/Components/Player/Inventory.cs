c1ebe53a1f78d3cd7dd754300e85e42cabc4282c
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-04-21 03:04:25 +0300

    Item placement

--- Assets/Scripts/Components/Player/Inventory.cs
+++ Assets/Scripts/Components/Player/Inventory.cs
@@ -8,12 +8,29 @@ namespace SupHero.Components.Character {
 
         // InGame weapon representation
         public Transform rightHand;
+        // Parts of the body
+        public Transform hairPlace;
+        public Transform eyesPlace;
+        public Transform leftForearmPlace;
+        public Transform leftHandPlace;
+        public Transform rightForearmPlace;
+        public Transform rightHandPlace;
+        public Transform chestPlace;
+        public Transform backPlace;
+        public Transform leftLegPlace;
+        public Transform leftShinPlace;
+        public Transform rightLegPlace;
+        public Transform rightShinPlace;
 
         public WeaponController primaryWeapon { get; private set; }
         public WeaponController secondaryWeapon { get; private set; }
 
         public ItemController firstItem { get; private set; }
         public ItemController secondItem { get; private set; }
+
+        // For item tests
+        public int firstItemId;
+        public int secondItemId;
 
         private PlayerController owner;
 
@@ -33,7 +50,12 @@ namespace SupHero.Components.Character {
         }
 
         public void setupItems() {
-
+            if (firstItemId >= 0) {
+                firstItem = equipItem(firstItemId);
+            }
+            if (secondItemId >= 0 && secondItemId != firstItemId) {
+                secondItem = equipItem(secondItemId);
+            }
         }
 
         public void processDrop(Entity type, int id) {
@@ -81,12 +103,54 @@ namespace SupHero.Components.Character {
             }
         }
 
-        public void equipItem(int id) {
-
+        public ItemController equipItem(int id) {
+            ItemData itemData = Data.Instance.getItemById(id);
+            if (itemData != null) {
+                foreach (BodySlot slot in itemData.placement) {
+                    if (slot == BodySlot.NONE) continue;
+                    Transform placement = getPlacement(slot);
+                    GameObject instance = Instantiate(itemData.prefab, placement.position, Quaternion.identity) as GameObject;
+                    instance.transform.SetParent(placement);
+                    instance.transform.localEulerAngles = itemData.prefab.transform.rotation.eulerAngles;
+                }
+                return itemData.prefab.GetComponent<ItemController>();
+            }
+            else return null;
         }
 
         public void useSupply(int id) {
 
+        }
+
+        private Transform getPlacement(BodySlot slot) {
+            switch (slot) {
+                case BodySlot.BACK:
+                    return backPlace;
+                case BodySlot.CHEST:
+                    return chestPlace;
+                case BodySlot.HEAD:
+                    return hairPlace;
+                case BodySlot.LEFT_FOREARM:
+                    return leftForearmPlace;
+                case BodySlot.LEFT_HAND:
+                    return leftHandPlace;
+                case BodySlot.LEFT_LEG:
+                    return leftLegPlace;
+                case BodySlot.LEFT_SHIN:
+                    return leftShinPlace;
+                case BodySlot.NOSE:
+                    return eyesPlace;
+                case BodySlot.RIGHT_FOREARM:
+                    return rightForearmPlace;
+                case BodySlot.RIGHT_HAND:
+                    return rightHand;
+                case BodySlot.RIGHT_LEG:
+                    return rightLegPlace;
+                case BodySlot.RIGHT_SHIN:
+                    return rightShinPlace;
+                default:
+                    return gameObject.transform;
+            }
         }
     }
 }

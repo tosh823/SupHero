7ee4941b1c13b690f0b67dce9dc6c7ef7c3b287e
AaroM2 <n3ylaa00@students.oamk.fi>
2016-05-17 13:16:10 +0300

    Updated hero and guard health bar canvases to include armor (healthbar on top of guards is not using the prefab so changes not visible on them). Also added more circular ground indicator assets with different symbol ornaments.

---
+++ Assets/Scripts/RunMovieTexture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class RunMovieTexture : MonoBehaviour {
+
+	public MovieTexture movie;
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

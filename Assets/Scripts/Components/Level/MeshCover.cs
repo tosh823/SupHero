a91f4aec0319da0116cc8dd9e0adeb011e4e32ee
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-05-20 22:39:34 +0300

    Destructable objects

---
+++ Assets/Scripts/Components/Level/MeshCover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SupHero.Components.Level {
+    public class MeshCover : BaseDestructable {
+
+        public Mesh halfDestroyed;
+        public Mesh fullDestroyed;
+
+        private float initialDurability;
+        private MeshFilter model;
+        
+        public override void Start() {
+            base.Start();
+            initialDurability = durability;
+            model = GetComponentInChildren<MeshFilter>();
+        }
+
+        public override void Update() {
+            base.Update();
+        }
+
+        public override void takeDamage(float damage) {
+            base.takeDamage(damage);
+            if (durability <= 0.2f * initialDurability) {
+                model.mesh = fullDestroyed;
+            }
+            else if (durability <= 0.6f * initialDurability) {
+                model.mesh = halfDestroyed;
+            }
+        }
+    }
+}

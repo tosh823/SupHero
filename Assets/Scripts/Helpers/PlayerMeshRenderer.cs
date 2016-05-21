b3639df3b6887e853d121667d19748bd28e7f9ac
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-05-21 12:53:11 +0300

    Fixed direction indicator

---
+++ Assets/Scripts/Helpers/PlayerMeshRenderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using SupHero.Components.Character;
+
+namespace SupHero.Components.Helpers {
+    public class PlayerMeshRenderer : MonoBehaviour {
+
+        private PlayerController player;
+
+        void Start() {
+            player = GetComponentInParent<PlayerController>();
+        }
+
+        void Update() {
+
+        }
+
+        void OnBecameVisible() {
+            player.stopTracing();
+        }
+
+        void OnBecauseInVisible() {
+            player.startTracing();
+        }
+    }
+}
